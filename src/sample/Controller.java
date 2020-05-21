ae5db2a1850ec335eb56d043aad48344ed5e97c0
baron-mc <baron_mc@yahoo.com>
2020-05-21 11:14:49 +0200

    Interface terminé

---
+++ src/sample/Controller.java
@@ -0,0 +1,11 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+    @FXML
+    private void stop(ActionEvent event) {
+        System.exit(0);
+    }
+}
