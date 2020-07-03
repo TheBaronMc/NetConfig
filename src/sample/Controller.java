8f9ef6038e21773e1c7858dfdbd581e89fdda43b
Charly-Ginevra <cy.ginevra@gmail.com>
2020-07-03 18:42:35 +0200

    Conversion OK

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Controller {
-    public ObservableList list = FXCollections.observableArrayList();
 
     // Mode button
     public RadioButton hexMode;
@@ -35,7 +34,7 @@ public class Controller {
     public TextField naa;
 
     // Will store errors
-    public ArrayList<String> errors;
+    public ArrayList<String> errors = new ArrayList<String>();
 
 
     public void initialize() {
@@ -81,17 +80,163 @@ public class Controller {
         /**
          * Convert an address into the current mode
          */
-        this.errors = new ArrayList<String>();
+        this.errors.clear();
+        // ADDRESS PART
         for (TextField field:
              noEmptyFields) {
             String address = field.getCharacters().toString();
-            if (checkAddress(address, oldMode)) {
+            if (checkAddress(address, oldMode) && !(field.getId().equals(this.mask.getId())) ) {
                 System.out.println("Good address");
+                String convertedAddress = convert(address, modeStringToInt(currentMode), modeStringToInt(oldMode));
+                field.setText(convertedAddress);
+            } else if (field.getId().equals(this.mask.getId())) { // in that the address is the mask
+                if (checkMask(address, oldMode)) {
+                    System.out.println("MASK : OK");
+                    String convertedAddress = convert(address, modeStringToInt(currentMode), modeStringToInt(oldMode));
+                    field.setText(convertedAddress);
+                } else {
+                    errors.add(field.getId());
+                    errors.add("Not valable mask");
+                }
             } else {
                 errors.add(field.getId());
                 errors.add("Not valable address");
             }
         }
+        // MASK PART
+
+        // ERROR PART
+        if (errors.size() != 0) {
+            String errorMessage = "Some values haven't been convert :\n ";
+            for (int i = 0; i < this.errors.size(); i = i + 2) {
+                errorMessage = errorMessage + errors.get(i) + " : " + errors.get(i + 1) + "\n ";
+            }
+            warnMessage(errorMessage, Alert.AlertType.WARNING);
+        }
+    }
+
+    private String convert(String address, int currentMode, int oldMode) {
+        String[] splitAddress;
+        String[] convertedAddressArray = new String[4];
+        String convertedAddress = "";
+
+        if (oldMode != 16) {
+            splitAddress = address.split("\\.");
+        } else {
+            splitAddress = address.split(":");
+        }
+
+        for (int i = 0; i < splitAddress.length; i++) {
+            if (currentMode == 10) {
+                convertedAddressArray[i] = Integer.toString(Integer.parseInt(splitAddress[i], oldMode));
+            } else if (currentMode == 16) {
+                convertedAddressArray[i] = Integer.toHexString(Integer.parseInt(splitAddress[i], oldMode));
+            } else if (currentMode == 2) {
+                convertedAddressArray[i] = Integer.toBinaryString(Integer.parseInt(splitAddress[i], oldMode));
+            }
+        }
+
+        if (currentMode == 2) {
+            for (int i = 0; i < convertedAddressArray.length; i++) {
+                while (convertedAddressArray[i].length() < 8) {
+                    convertedAddressArray[i] = "0" + convertedAddressArray[i];
+                }
+            }
+        } else if (currentMode == 16) {
+            for (int i = 0; i < convertedAddressArray.length; i++) {
+                if (convertedAddressArray[i].length() == 1) {
+                    convertedAddressArray[i] = "0" + convertedAddressArray[i];
+                }
+            }
+        }
+
+        for (int i = 0; i < convertedAddressArray.length; i++) {
+            if (currentMode != 16) {
+                System.out.println(convertedAddress);
+                convertedAddress = convertedAddress +convertedAddressArray[i] + ".";
+            } else {
+                System.out.println(convertedAddress);
+                convertedAddress = convertedAddress + convertedAddressArray[i] + ":";
+            }
+        }
+
+        return convertedAddress.substring(0, (convertedAddress.length()-1));
+    }
+
+    private boolean checkMask(String mask, String oldMode) {
+        /**
+         * Check if the mask in param is a valable one
+         */
+        String[] splitMask;
+        String[] binMask = new String[4];
+
+        if (oldMode.equals("HEX")) {
+            splitMask = mask.split(":");
+            if (splitMask.length != 4) {
+                return false;
+            }
+            for (int i=0; i < splitMask.length; i++) {
+                try {
+                    if (!(0 <= Integer.parseInt(splitMask[i], 16) && Integer.parseInt(splitMask[i], 16) <= 255)) {
+                        return false;
+                    }
+                } catch (Exception e) {
+                    return false;
+                }
+                binMask[i] = Integer.toBinaryString(Integer.parseInt(splitMask[i], 16));
+            }
+        } else {
+            splitMask = mask.split("\\.");
+            if (splitMask.length != 4) {
+                return false;
+            }
+            if (oldMode.equals("DEC")) {
+                for (int i=0; i < splitMask.length; i++) {
+                    try {
+                        if (!(0 <= Integer.parseInt(splitMask[i]) && Integer.parseInt(splitMask[i]) <= 255)) {
+                            return false;
+                        }
+                    } catch (Exception e) {
+                        return false;
+                    }
+                    binMask[i] = Integer.toBinaryString(Integer.parseInt(splitMask[i]));
+                }
+            } else if (oldMode.equals("BIN")) {
+                for (int i=0; i < splitMask.length; i++) {
+                    try {
+                        if (!(0 <= Integer.parseInt(splitMask[i], 2) && Integer.parseInt(splitMask[i], 2) <= 255)) {
+                            return false;
+                        }
+                    } catch (Exception e) {
+                        return false;
+                    }
+                }
+                binMask = splitMask;
+            }
+        }
+
+        for (int i=0; i < binMask.length; i++) {
+            while (binMask[i].length() < 8) {
+                binMask[i] = "0" + binMask[i];
+            }
+        }
+        if (binMask[0].startsWith("0")) {
+            return false;
+        }
+
+        // LAST CHECK
+        boolean x = false;
+        for (String elt: binMask) {
+            for (char bit: elt.toCharArray()) {
+                if (bit == '1' && x) {
+                    return false;
+                } else if (bit != '1') {
+                    x = true;
+                }
+            }
+        }
+
+        return true;
     }
 
     private boolean checkAddress(String address, String mode) {
@@ -183,5 +328,17 @@ public class Controller {
             return  "BIN";
         }
         return null;
+    }
+
+    public int modeStringToInt(String mode) {
+        if (mode == "DEC") {
+            return 10;
+        } else if (mode == "HEX") {
+            return 16;
+        } else if (mode == "BIN") {
+            return 2;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 }
