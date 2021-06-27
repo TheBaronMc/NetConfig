5162c17b4627c89429835c71565603b450d47a31
TheBaronMC <baron_mc@yahoo.com>
2021-06-27 18:40:56 +0200

    new main

--- src/main/java/org/netconfig/App.java
+++ src/main/java/org/netconfig/App.java
@@ -5,13 +5,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.netconfig.graphic.UIController;
+import org.netconfig.model.NetConfig;
 
 
 public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/NetConfig.fxml"));
+        NetConfig model = new NetConfig();
+        UIController controller = new UIController(model);
+        model.addPropertyChangeListener(controller);
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/NetConfig.fxml"));
+        loader.setController(controller);
+
+        Parent root = loader.load();
         primaryStage.setTitle("NetConfig");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
